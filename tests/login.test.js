a238e0446fc019dc90b66731122b7241117ad88c
Samuel Aquino <samuelssaquino@outlook.com>
2026-04-01 22:44:21 +0100

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,13 +1,14 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
+import { pegarBaseURL } from '../utils/variaveis.js';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
     stages: [
         { duration: '5s', target: 10 },
         { duration: '20s', target: 10 },
-        { duration: '5s', target: 0 },        
-    ],  
+        { duration: '5s', target: 0 },
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
@@ -15,9 +16,8 @@ export const options = {
 }
 
 export default function () {
-    const url = 'http://localhost:3000/login';
+    const url = pegarBaseURL() + '/login';
 
-    console.log(postLogin)
     const payload = JSON.stringify(postLogin);
 
     const params = {
@@ -29,8 +29,8 @@ export default function () {
     const res = http.post(url, payload, params)
 
     check(res, {
-        'Validar que o status é 200': (r) =>  r.status === 200,
-        'Validar que o token é string': (r) => typeof(r.json().token) == 'string',
+        'Validar que o status é 200': (r) => r.status === 200,
+        'Validar que o token é string': (r) => typeof (r.json().token) == 'string',
     })
 
     sleep(1)
